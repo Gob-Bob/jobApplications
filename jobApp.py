37bcdd6a428dc6f71c4f495442598a248cb07ac3
Tyler <nguyentyler97@gmail.com>
2022-08-16 20:49:37 -0700

    Comment basic code layout

---
+++ jobApp.py
@@ -0,0 +1,26 @@
+# If there is already a browser window opened on the right monitor, 
+    # Open a new tab and continue program
+# If there is NOT already a browser window opened on the right monitor (empty screen),
+    # Search up Google Chrome in searchbar and open
+    # If browser is opened on another screen windowed,
+        # Move it to the right monitor
+        # Full screen the window
+# Select the task bar and input link to Google Drive Cover Letter folder
+
+# Right click on first item and select make a copy
+# Wait for duplicate process to finish
+# Open the file
+# At the name on top, remove "Copy of" and replace old position with new position
+
+# Go to previous tab using keyboard shortcut (back to Google Drive)
+# Go back to Job Hunt using backspace shortcut
+# Open Resume
+
+# Right click on first item and select make a copy
+# Wait for duplicate process to finish
+# Open the file
+# At the name on top, remove "Copy of" and replace old position with new position
+
+# Allow user to review and make changes to both documents
+# Once user has indicated that they are done,
+# 
